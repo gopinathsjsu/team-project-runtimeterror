c1c78a4b9397a23e5b70447caf4bfd06bf33664a
Rajat Banerjee <rajatmig29@gmail.com>
2022-04-07 16:16:17 -0700

    Adding Room factory

---
+++ hm-service/src/main/java/com/example/hmservice/hotel/RoomTypeSingle.java
@@ -0,0 +1,32 @@
+package com.example.hmservice.hotel;
+
+import java.util.HashMap;
+
+public class RoomTypeSingle extends Hotel {
+    HashMap<String, Double> gcCostMultiplier = new HashMap<>();
+
+
+    public RoomTypeSingle(Integer gc) {
+        guestCount = gc;
+        //TODO: Read from DB
+        basePrice = 90.00;
+        gcCostMultiplier.put("1", 1.00);
+        gcCostMultiplier.put("2", 1.10);
+        gcCostMultiplier.put("3", 1.20);
+        gcCostMultiplier.put("4", 1.25);
+    }
+
+    @Override
+    public String getPriceBreakdown() {
+        return hotelName + "\n Room: Queen \n Guests:" + this.guestCount;
+    }
+
+    @Override
+    public Double getCost() {
+        //consider the number of guests.
+        //get cost of room type, and guest limitation for room type
+        //auto-select number of rooms to accommodate guests
+        Double multiplier = gcCostMultiplier.get(this.guestCount.toString());
+        return basePrice * multiplier;
+    }
+}
