70929383ad1fc9b7bf2c9ab1c91f46b2faac82b1
Soham Bhattacharjee <soham.onlyn@gmail.com>
2022-04-10 11:43:58 -0700

    UI elements added

--- hm-web/src/App.js
+++ hm-web/src/App.js
@@ -4,6 +4,8 @@ import './styles/App.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './Footer';
+import SignIn from './Login/SingIn';
+import SignUp from './Login/SignUp';
 
 function App() {
   return (
@@ -14,7 +16,8 @@ function App() {
           <div className='bodyContainer'>
             <Routes>
               <Route path="/" element={<Landing />} />
-              <Route path="/listings" element={<Landing />} />
+              <Route path="/login" element={<SignIn />} />
+              <Route path="/signup" element={<SignUp />} />
             </Routes>
           </div>
         </BrowserRouter>
