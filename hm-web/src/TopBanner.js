0b37ac9c0425e30baf3ace01e1312b42ae2eb5b0
Soham Bhattacharjee <soham.onlyn@gmail.com>
2022-04-10 13:03:46 -0700

    more ui stuff

--- hm-web/src/TopBanner.js
+++ hm-web/src/TopBanner.js
@@ -1,12 +1,44 @@
 import topbanner from './styles/TopBanner.module.css';
 import Paper from '@mui/material/Paper';
+import { Link } from 'react-router-dom'
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import { Grid } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useLocation } from 'react-router-dom';
 
 export default function TopBanner() {
+    const matches = useMediaQuery('(min-width:600px)');
+    const location = useLocation()
     return (
         <Paper elevation={2} className={topbanner.header}>
-            <h1>
-                <a className={topbanner.link} href="/">Hotels</a>
-            </h1>
+            <Box
+                component="header"
+                sx={{
+                    py: 2,
+                    backgroundColor: 'white',
+                }}
+            >
+                <Container maxWidth="lg">
+                    <Grid container sx={{ dispalay: 'flex', justifyContent: 'space-between' }}>
+                        <Grid item>
+                            <Typography variant="h4" display="block">
+                                <Link className={topbanner.link} to="/">Hotel</Link>
+                            </Typography>
+                        </Grid>
+                        {location.pathname === `/` &&
+                            <Grid item>
+                                <Link className={topbanner.singInLink} to="/login">
+                                    <PersonIcon />
+                                    {matches ? `Sign In` : ``}
+                                </Link>
+                            </Grid>
+                        }
+                    </Grid>
+                </Container>
+            </Box>
         </Paper>
     );
 }
