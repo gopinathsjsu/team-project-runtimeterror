a6df6c0b697ff3647c1eb6a7e7656c2127ceba2f
Soham Bhattacharjee <soham.onlyn@gmail.com>
2022-04-10 11:43:58 -0700

    UI elements added

---
+++ hm-web/src/Login/SignUp.js
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import { Link } from 'react-router-dom';
+import * as MaterialLink from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { Paper } from '@mui/material';
+
+
+export default function SignUp() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <Paper elevation={4}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <PersonAddIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign Up
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Container sx={{ display: 'flex', justifyContent: 'center' }} fixed>
+              <Link to="/login">
+                <MaterialLink.default variant="body2">
+                  {"Back to login"}
+                </MaterialLink.default>
+              </Link>
+            </Container>
+          </Box>
+        </Box>
+      </Container>
+    </Paper>
+  );
+}
