b4db5f86ef463d9efb30e7f1ff2efc0fceaaf0f1
Rajat Banerjee <rajatmig29@gmail.com>
2022-04-10 11:23:19 -0700

    Adding booking api

---
+++ hm-service/src/main/java/com/hmservice/hotel/models/Booking.java
@@ -0,0 +1,120 @@
+package com.hmservice.hotel.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "booking")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bookingId")
+    private Long bookingId;
+
+    @Column(name = "userId")
+    private Long userId;
+
+    @Column(name = "hotelId")
+    private Integer hotelId;
+
+    @Column(name = "roomId")
+    private Integer roomId;
+
+    @Column(name = "bookingDate")
+    private String bookingDate;
+
+    @Column(name = "checkInDate")
+    private String checkInDate;
+
+    @Column(name = "checkOutDate")
+    private String checkOutDate;
+
+    @Column(name = "roomTypeCode")
+    private String roomTypeCode;
+
+    @Column(name = "guestCount")
+    private Integer guestCount;
+
+    @Column(name = "totalPrice")
+    private Integer totalPrice;
+
+    public Long getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(Long bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Integer getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(Integer hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public Integer getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Integer roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(String bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public String getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(String checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public String getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(String checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public String getRoomTypeCode() {
+        return roomTypeCode;
+    }
+
+    public void setRoomTypeCode(String roomTypeCode) {
+        this.roomTypeCode = roomTypeCode;
+    }
+
+    public Integer getGuestCount() {
+        return guestCount;
+    }
+
+    public void setGuestCount(Integer guestCount) {
+        this.guestCount = guestCount;
+    }
+
+    public Integer getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Integer totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
