2640f0008a0473519eac6f89a51162305181d5f7
Soham Bhattacharjee <soham.onlyn@gmail.com>
2022-04-09 18:03:17 -0700

    counter

--- hm-web/src/LandingSearch/Travellers.js
+++ hm-web/src/LandingSearch/Travellers.js
@@ -2,21 +2,62 @@ import React from 'react'
 import TextField from '@mui/material/TextField';
 import PersonIcon from '@mui/icons-material/Person';
 import InputAdornment from '@mui/material/InputAdornment';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Counter from './Counter'
+import Stack from '@mui/material/Stack';
+import { Button } from '@mui/material';
 
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+};
 export default function Travellers() {
-  return (<TextField
-    fullWidth={true}
-    id="outlined-read-only-input"
-    label="Travellers"
-    defaultValue="1 room, 2 travellers"
-    InputProps={{
-      startAdornment: (
-        <InputAdornment position="start">
-          <PersonIcon />
-        </InputAdornment>
-      ),
-      readOnly: true,
-    }}
-  />)
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [travellers, setTravellers] = React.useState(2);
+  const [rooms, setRooms] = React.useState(1);
+  return (<>
+    <TextField
+      fullWidth={true}
+      onClick={handleOpen}
+      id="outlined-read-only-input"
+      label="Travellers"
+      value={`${rooms} room${rooms > 1 ? `s` : ``}, ${travellers} traveller${travellers > 1 ? `s` : ``}`}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <PersonIcon />
+          </InputAdornment>
+        ),
+        readOnly: true,
+      }}
+    />
+    <Modal
+      open={open}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Travellers
+        </Typography>
+        <Stack spacing={3} mt pt>
+          <Box> Rooms: <Counter value={rooms} handleChange={setRooms} /></Box>
+          <Box> Guests: <Counter value={travellers} handleChange={setTravellers} /></Box>
+          <Button variant="outlined" onClick={handleClose}>
+              Done
+            </Button>
+        </Stack>
+      </Box>
+    </Modal>
+  </>)
 }
