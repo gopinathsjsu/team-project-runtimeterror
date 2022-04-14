89cc93ebe0f0518e071814576c02ebaa4c040cc2
Soham Bhattacharjee <soham.onlyn@gmail.com>
2022-04-14 15:20:35 -0700

    booking

--- hm-web/src/Actions/bookingAction.js
+++ hm-web/src/Actions/bookingAction.js
@@ -38,3 +38,4 @@ export const selectAminities = (amenities) => dispatch => {
     payload: amenities
   })
 }
+
--- hm-web/src/Booking/Confirmation.js
+++ hm-web/src/Booking/Confirmation.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux';
 import Paper from '@mui/material/Paper';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Button, Container } from '@mui/material';
 import styles from '../styles/booking.module.css'
 import { find, isEmpty } from 'lodash';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 import Hotel from './Hotel'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -12,7 +12,7 @@ import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
 import Counter from '../LandingSearch/Counter';
 import { AMENITIES_LIST } from '../helpers/constants';
-import { calculatePrice } from '../helpers/API'
+import { calculatePrice, bookHotel } from '../helpers/API'
 import Cookies from 'js-cookie';
 
 const currencyFormatter = new Intl.NumberFormat('en-US', {
@@ -37,6 +37,7 @@ const mapDispatchToProps = () => ({
 })
 
 function Confirmation(props) {
+  const navigate = useNavigate()
   const { selectedAmenities = [] } = props
   const [price, setPrice] = useState()
   const [priceBreakdown, setPriceBreakdown] = useState()
@@ -46,15 +47,14 @@ function Confirmation(props) {
   })
   const [localAmenityCount, setLocalAmenityCount] = useState(localAmMap)
 
-  const calculatePriceEstimate = async () => {
+  const getBookingPayload = () => {
     const amenities = Object.keys(localAmenityCount).map(key => {
       return {
         amenityCode: key,
         count: localAmenityCount[key]
       }
     })
-
-    const { data } = await calculatePrice({
+    return {
       userId,
       checkInDate,
       checkOutDate,
@@ -64,10 +64,21 @@ function Confirmation(props) {
       roomId: selectedRoom.roomTypeId,
       hotelId: hotelDetails.id,
       amenities
-    })
+    }
+  }
+
+  const calculatePriceEstimate = async () => {
+    const payload = getBookingPayload()
+    const { data } = await calculatePrice(payload)
     const { bookingTotal, bookingDetails } = data
     setPrice(currencyFormatter.format(bookingTotal))
     setPriceBreakdown(bookingDetails)
+  }
+
+  const confirmBooking = async () => {
+    const payload = getBookingPayload()
+    const { data } = await bookHotel(payload)
+    debugger
   }
 
   useEffect(() => {
@@ -123,7 +134,7 @@ function Confirmation(props) {
             </Grid>
             {
               selectedAmenities.length > 0 ?
-                <Grid item xs={12} md={6}>
+                <Grid item xs={12}>
                   <Item>
                     {selectedAmenities.map(sel => {
                       const amenity = find(AMENITIES_LIST, a => a.amenityCode === sel.amenityCode)
@@ -137,7 +148,7 @@ function Confirmation(props) {
                 </Grid>
                 : null
             }
-            <Grid item xs={12} md={6}>
+            <Grid item xs={12}>
               <Item>
                 <Typography variant="h5" component="div">
                   Price: <b>{price}</b>
@@ -149,6 +160,12 @@ function Confirmation(props) {
                   <span dangerouslySetInnerHTML={{ __html: priceBreakdown }}></span>
                 </Box>
               </Item>
+            </Grid>
+            <Grid item xs={12}>
+              <Container sx={{ display: 'flex', justifyContent: 'space-around' }}>
+                <Button variant="outlined" onClick={() => { navigate(`/book`) }}>Edit</Button>
+                <Button onClick={() => { confirmBooking() }} variant="outlined" size="medium">Confirm</Button>
+              </Container>
             </Grid>
           </Grid>
         </CardContent>
